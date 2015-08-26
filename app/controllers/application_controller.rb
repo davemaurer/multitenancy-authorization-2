163650aef5ba69edd19e8a200b6cbdf1638dd580
David Maurer <davemaurermail@gmail.com>
2015-08-26 09:51:35 -0600

    add methods and callback to application_controller.rb and methods to permission service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :authorize!
+
   protect_from_forgery with: :exception
 
   add_flash_types :success, :info, :warning, :danger
 
   helper_method :current_user
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_permission
+    @current_permission ||= PermissionService.new(current_user)
+  end
+
+  def authorize!
+    unless current_permission.allow?(params[:controller], params[:action])
+      redirect_to root_url, danger: "You don't know me."
+    end
   end
 end
--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -2,9 +2,10 @@ class PermissionService
   attr_reader :user
 
   def initialize(user)
+    @user = user
   end
 
   def allow?(controller, action)
-
+    return true if controller == 'stores' && action == 'index'
   end
 end
