bc8abfa6332d0a5644175b3161895aed782ee6a1
David Maurer <davemaurermail@gmail.com>
2015-08-26 11:13:45 -0600

    hide links for unauthorized users by adding conditional to view and methods in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def registered_user?
+    current_user && current_user.registered_user?
+  end
+
+  def platform_admin?
+
+  end
 end
--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -10,7 +10,11 @@ class PermissionService
   def allow?(controller, action)
     @controller = controller
     @action = action
-    if user && user.registered_user?
+    if user && user.platform_admin?
+      platform_admin_permissions
+    elsif user && user.store_admin?
+      store_admin_permissions
+    elsif user && user.registered_user?
       registered_user_permissions
     else
       guest_user_permissions
@@ -18,6 +22,18 @@ class PermissionService
   end
 
   private
+
+  def platform_admin_permissions
+    return true if controller == 'users' && action.in?(%w(index show new create destroy))
+    store_admin_permissions
+  end
+
+  def store_admin_permissions
+    return true if controller == 'stores' && action.in?(%w(index show))
+    return true if controller == 'sessions' && action.in?(%w(new create destroy))
+    return true if controller == 'items' && action.in?(%w(index show new create destroy))
+    return true if controller == 'orders' && action.in?(%w(index show new create destroy))
+  end
 
   def registered_user_permissions
     return true if controller == 'stores' && action.in?(%w(index show))
